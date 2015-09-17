31a5911f079a03d2f1a8a27a4f60f422b89d2cf2
= <sebbekarlsson97@gmail.com>
2015-09-17 21:28:07 +0200

    First

---
+++ flaskr/flaskr.py
@@ -0,0 +1,43 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+
+
+DATABASE = '/tmp/robertbjork.db'
+DEBUG = True
+USERNAME = 'root'
+PASSWORD = 'tango255'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
