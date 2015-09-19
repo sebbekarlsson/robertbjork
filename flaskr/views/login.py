fc8a6188f73c860e3e82615296f12ae779bdab0e
= <sebbekarlsson97@gmail.com>
2015-09-19 22:26:26 +0200

    WTForms implementation on login

--- flaskr/views/login.py
+++ flaskr/views/login.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
+
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 
 login = Blueprint('login', __name__,
                         template_folder='templates')
 
-@login.route('/login')
+class MyForm(Form):
+    email = StringField('Email', validators=[Email(message=u'Invalid email address.')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+@login.route('/login', methods=('GET', 'POST'))
 def login_blueprint():
-    return render_template('login.html')
+    form = MyForm(csrf_enabled=False)
+    form.validate_on_submit()
+    return render_template('login.html', form=form)
